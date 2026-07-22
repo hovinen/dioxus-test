cfb518961092e60e5ee1a6a0d792b4cc08372059
Bradford Hovinen <bradford@hovinen.me>
2026-07-22 21:26:50 +0200

    Support querying elements by role
    This uses the accessibility tree as obtained through accesskit.

--- src/query.rs
+++ src/query.rs
@@ -1,4 +1,5 @@
 use crate::TesterError;
+use accesskit::{Node, Role};
 use blitz_dom::{Document as _, SelectorList};
 use dioxus_native_dom::DioxusDocument;
 use style::dom_apis::{MayUseInvalidation, QueryFirst, query_selector};
@@ -243,4 +244,150 @@ fn render_parent_dom(parent: Option<&dyn Query>, document: &DioxusDocument) -> S
         },
         None => document.inner().root_element().outer_html_pretty(),
     }
+}
+
+#[derive(Clone)]
+struct QueryByRole<'parent>(Role, Option<&'parent dyn Query>);
+
+impl<'parent> Query for QueryByRole<'parent> {
+    fn get_first_element(&self, document: &DioxusDocument) -> Option<usize> {
+        let tree = document.inner.borrow().build_accessibility_tree();
+        let starting_node_id = self.get_starting_node_id(document)?;
+        self.find_first_element_starting_at(accesskit::NodeId(starting_node_id as u64), &tree.nodes)
+    }
+
+    fn get_all_elements(&self, document: &DioxusDocument) -> Vec<usize> {
+        let tree = document.inner.borrow().build_accessibility_tree();
+        let Some(starting_node_id) = self.get_starting_node_id(document) else {
+            return vec![];
+        };
+        self.find_all_elements_starting_at(accesskit::NodeId(starting_node_id as u64), &tree.nodes)
+    }
+
+    fn render_parent_dom(&self, document: &DioxusDocument) -> String {
+        match self.1 {
+            Some(c) => match c.get_first_element(document) {
+                Some(element) => document
+                    .inner()
+                    .get_node(element)
+                    .expect("Expected to find node")
+                    .outer_html_pretty(),
+                None => c.render_parent_dom(document),
+            },
+            None => document.inner().root_element().outer_html_pretty(),
+        }
+    }
+
+    fn describe_failure(&self, document: &DioxusDocument) -> TesterError {
+        if let Some(parent) = self.1
+            && parent.get_first_element(document).is_none()
+        {
+            parent.describe_failure(document)
+        } else {
+            TesterError::NoSuchElementWithRole(
+                format!("{:?}", self.0),
+                self.render_parent_dom(document),
+            )
+        }
+    }
+}
+
+impl<'parent> QueryByRole<'parent> {
+    fn get_starting_node_id(&self, document: &DioxusDocument) -> Option<usize> {
+        if let Some(parent) = &self.1 {
+            parent.get_first_element(document)
+        } else {
+            Some(document.inner.borrow().root_node().id)
+        }
+    }
+
+    fn find_first_element_starting_at(
+        &self,
+        node_id: accesskit::NodeId,
+        nodes: &[(accesskit::NodeId, Node)],
+    ) -> Option<usize> {
+        let (_, node) = nodes.iter().find(|(id, _)| id.0 == node_id.0)?;
+        if node.role() == self.0 {
+            Some(node_id.0 as usize)
+        } else {
+            node.children()
+                .iter()
+                .find_map(|child_id| self.find_first_element_starting_at(*child_id, nodes))
+        }
+    }
+
+    fn find_all_elements_starting_at(
+        &self,
+        node_id: accesskit::NodeId,
+        nodes: &[(accesskit::NodeId, Node)],
+    ) -> Vec<usize> {
+        let Some((_, node)) = nodes.iter().find(|(id, _)| id.0 == node_id.0) else {
+            return vec![];
+        };
+        let mut result: Vec<_> = node
+            .children()
+            .iter()
+            .filter_map(|child_id| self.find_first_element_starting_at(*child_id, nodes))
+            .collect();
+        if node.role() == self.0 {
+            result.push(node_id.0 as usize)
+        }
+        result
+    }
+}
+
+impl<'parent> ParentableQuery for QueryByRole<'parent> {
+    fn with_parent(self, parent: &dyn Query) -> impl CloneableQuery {
+        QueryByRole(self.0, Some(parent))
+    }
+}
+
+impl<'parent> std::fmt::Display for QueryByRole<'parent> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, r#"role="{:?}"#, self.0)
+    }
+}
+
+impl<'parent> IntoQuery for QueryByRole<'parent> {
+    type Query = Self;
+
+    fn into_query(self) -> Self::Query {
+        self
+    }
+}
+
+/// Returns a query selector matching elements with the given ARIA role.
+///
+/// ```
+/// use accesskit::Role;
+/// use dioxus::prelude::*;
+/// use dioxus_test::{by_role, matchers::{eq, inner_html}, render};
+///
+/// #[component]
+/// fn MyComponent() -> Element {
+///     rsx! {
+///         button {
+///              onclick: |_| {
+///                  print!("Clicked!")
+///              },
+///              "Click me!"
+///         }
+///     }
+/// }
+///
+/// # async fn test_fn() {
+/// let tester = render(MyComponent).build();
+/// tester
+///     .query(by_role(Role::Button))
+///     .click()
+///     .await
+///     .unwrap();
+/// # }
+/// # tokio::runtime::Builder::new_current_thread().enable_time().build().unwrap().block_on(test_fn());
+/// ```
+///
+/// This attribute is a common convention for marking DOM components with which tests interact. Find
+/// more information [here](https://testing-library.com/docs/queries/bytestid/).
+pub fn by_role(role: Role) -> impl IntoQuery {
+    QueryByRole(role, None)
 }
